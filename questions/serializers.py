1a6a9034b2eb072ffc3d8feb28bd3a9c8300700a
Amanda Muniz <amanda.muniiz@outlook.com>
2017-10-05 21:37:55 -0300

    Deleting questions folder

--- questions/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Questions
-
-class QuestionsSerializer (serializers.Serializer):
-    class Meta:
-        model = Questions
-        fields = ('QuestionId', 'QuestionAnswer')

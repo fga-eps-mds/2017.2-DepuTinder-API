253e5f1d8dc4009bb5d17704bf9635e6da0366b8
gustavo2795 <gustavo.unb27@gmail.com>
2017-10-10 20:34:05 -0300

    Adding Parlamentary model
    Related to issue #16
    
    Signed-off-by: lucianaribeiro <albuquerqueluciana54@gmail.com>
    Signed-off-by: gustavo2795 <gustavo.unb27@gmail.com>

--- parlamentary/models.py
+++ parlamentary/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Parlamentarians(models.Model):
+    candidadeName = models.CharField(max_length=150, blanck=False)
+    candidateDescription = models.CharField(max_length=500, blanck=True)
+
+# In the future will be add two more atributes: State Foreing key and PoliticalParty Foreign key.

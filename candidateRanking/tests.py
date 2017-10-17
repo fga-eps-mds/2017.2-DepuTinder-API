4e840a1711aa9a484696b03ed017b82ccb781c8a
byronkamal <byronkamal@hotmail.com>
2017-10-17 21:58:20 -0200

    Testing candidateRaking View

--- candidateRanking/tests.py
+++ candidateRanking/tests.py
@@ -1,3 +1,13 @@
+from .views import rankingIndex
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
 
-# Create your tests here.
+class CandidateRankingTest(TestCase):
+    def testRequestFromAPI(self):
+        STATUS_CODE_APPROVED = int(200)
+
+        factory = APIRequestFactory()
+        request = factory.get('/ranking/')
+        response = rankingIndex(request)
+
+        self.assertEqual(response.status_code, STATUS_CODE_APPROVED)

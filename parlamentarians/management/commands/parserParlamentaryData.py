438c5bc76eb854dc75062c9461be35296797e952
Tulio Coelho <tulioopc@gmail.com>
2017-10-18 15:24:10 -0200

    Refactoring method for parsing the parlamentaryData

--- parlamentarians/management/commands/parserParlamentaryData.py
+++ parlamentarians/management/commands/parserParlamentaryData.py
@@ -8,10 +8,18 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         parlamentariansFile = open("parlamentariansData.csv", "r")
         csv_reader = csv.reader(parlamentariansFile)
+        row = next(csv_reader) # Jumping the header
+        self.parseData(row, csv_reader)
+
+    def parseData(self, row, csv_reader):
         for row in csv_reader:
-            _, created = Parlamentarians.objects.get_or_create(
-            parlamentaryPhotoPath = row[0],
-            parlamentaryName = row[1],
-            parlamentaryUF = row[2],
-            parlamentaryPoliticalParty = row[3],
-        )
+            parlamentary, created = Parlamentarians.objects.get_or_create(
+                parlamentaryPhotoPath = row[0],
+                parlamentaryName = row[1],
+                parlamentaryUF = row[2],
+                parlamentaryPoliticalParty = row[3],
+            )
+            if (created):
+                self.stdout.write("Deputado(a) " + parlamentary.parlamentaryName + " salvo(a) com sucesso!")
+            else:
+                self.stdout.write("Deputado(a) " + parlamentary.parlamentaryName + " não foi salvo no banco de dados!")

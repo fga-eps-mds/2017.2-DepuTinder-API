23951468afd47e1f81777fd2fb1a04df72157367
Tulio Coelho <tulioopc@gmail.com>
2017-10-18 15:35:17 -0200

    Refactoring parlamentary data request script

--- parlamentarians/management/commands/requestParlamentaryData.py
+++ parlamentarians/management/commands/requestParlamentaryData.py
@@ -6,23 +6,31 @@ from urllib.request import urlopen
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        self.stdout.write("Fazendo request dos dados no portal da camara")
-        response_data = urlopen("https://dadosabertos.camara.leg.br/api/v2/deputados?siglaPartido=psol&ordenarPor=nome").read().decode('utf-8')
-        parlamentariansData = json.loads(response_data)
+        url_path = "https://dadosabertos.camara.leg.br/api/v2/deputados?siglaPartido=psol&ordenarPor=nome"
+        parlamentariansData = self.request_data(url_path)
+        csv_file = self.create_csv_file()
+        self.parsing_data_to_csv(csv_file, parlamentariansData)
 
-        #creating the file
-        self.stdout.write("Abrindo arquivo csv")
+    def request_data(self, url_path):
+        self.stdout.write("Fazendo request dos dados no portal da camara")
+        response_data = urlopen(url_path).read().decode('utf-8')
+        parlamentariansData = json.loads(response_data)
+        return parlamentariansData
+
+    def create_csv_file(self):
+        self.stdout.write("Criando arquivo csv")
         parlamentariansFile = open("parlamentariansData.csv", "w")
         csv_file = csv.writer(parlamentariansFile)
         csv_file.writerow(["Link da Foto", "Nome", "UF", "Partido"])
+        return csv_file
 
-        #creating the objects
+    def parsing_data_to_csv(self, csv_file, parlamentariansData):
         self.stdout.write("Adicionando dados no arquivo csv")
-        for deputy in parlamentariansData["dados"]:
+        for parlamentary in parlamentariansData["dados"]:
             csv_file.writerow([
-                deputy["urlFoto"],
-                deputy["nome"],
-                deputy["siglaUf"],
-                deputy["siglaPartido"]
+                parlamentary["urlFoto"],
+                parlamentary["nome"],
+                parlamentary["siglaUf"],
+                parlamentary["siglaPartido"]
             ])
         self.stdout.write("Arquivo csv criado com sucesso!")

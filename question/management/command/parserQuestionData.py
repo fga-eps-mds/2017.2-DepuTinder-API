23ae5416263b3fc1f8ab1dc5e521b0774f32a861
gustavosinacio <gsinacio94@gmail.com>
2017-10-30 21:29:13 -0200

    Creating question model and parserQuestionData

---
+++ question/management/command/parserQuestionData.py
@@ -0,0 +1,25 @@
+from question.models import Question
+from propositions.models import Propositions
+from django.core.management import BaseCommand
+
+NUMBER_OF_QUESTIONS = 10
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        self.parseData()
+
+        def parseData(self):
+            propositions = Propositions.objects.all()
+
+            for proposition in propositions:
+                question, created = Question.objects.get_or_create(
+                    questionTitle = proposition.propositionTitle,
+                    questionSubtitle = proposition.propositionSubTitle,
+                    questionDescription = proposition.propositionDescription,
+                    questionAuthor = proposition.propositionAuthor,
+                    questionLink = proposition.propositionLink,
+                )
+            if (created):
+                self.stdout.write("Questao " + question.questionTitle + " salva com sucesso!")
+            else:
+                self.stdout.write("Questao " + question.questionTitle + " nao foi salva no banco de dados!")
---
+++ question/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+TITLE_LENGTH = 100
+SUBTITLE_LENGTH = 150
+DESCRIPTION_LENGTH = 200
+AUTHOR_LENGTH = 50
+LINK_LENGTH = 5000
+
+class Question(models.Model):
+    proposition = models.ForeignKey(
+        'propositions.Propositions',
+        on_delete=models.CASCADE,
+        related_name='proposition',
+        default=0
+    )
+    questionTitle = models.CharField(max_length=TITLE_LENGTH, blank=True)
+    questionSubtitle = models.CharField(max_length=SUBTITLE_LENGTH, blank=True)
+    questionDescription = models.CharField(max_length=DESCRIPTION_LENGTH, blank=True)
+    questionAuthor = models.CharField(max_length=AUTHOR_LENGTH, blank=True)
+    questionLink = models.CharField(max_length=AUTHOR_LENGTH, blank=True)

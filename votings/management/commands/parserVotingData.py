2c82578948ec8576cf84f9a62d03299d4947bc94
vitorcx <vitorcx010@gmail.com>
2017-10-30 11:55:46 -0200

    Removing unused code from parserVotingData

--- votings/management/commands/parserVotingData.py
+++ votings/management/commands/parserVotingData.py
@@ -19,44 +19,10 @@ class Command(BaseCommand):
             votings, created = Votings.objects.get_or_create(
                 #Gera inteiro para votação: -1 = NAO / 0 = ME ABSTENHO / 1 = SIM
                 candidateVote = random.randrange(-1,2),
-                #Gera todos os campos de um objeto Parlamentarians
-                candidateID = self.generateParlamentariansInstance(
-                    fake.image_url(),
-                    StringGenerator('[\l][\l]').render(),
-                    fake.name(),
-                    StringGenerator('[\l][\l]').render()
-                ),
-                #Gera todos os campos de um objeto Propositions
-                propositionID = self.generatePropositionInstance(
-                    random.randrange(1,100),
-                    fake.sentence(nb_words=5, variable_nb_words=True, ext_word_list=None),
-                    fake.sentence(nb_words=10, variable_nb_words=True, ext_word_list=None),
-                    fake.text(max_nb_chars=150, ext_word_list=None),
-                    fake.name(),
-                    fake.url()
-                ),
+                candidateID = 
+                propositionID =
             )
             if (created):
                 self.stdout.write("Voting " + str(votings.candidateVote) + " salvo com sucesso!")
             else:
                 self.stdout.write("Voting " + str(votings.candidateVote) + " erro ao salvar!")
-
-    #Instancia Propositions e Questionnaire
-    def generatePropositionInstance(self, questionnaireID, propositionTitle, propositionSubTitle, propositionDescription, propositionAuthor, propositionLink):
-        questionnaire = Questionnaire()
-        proposition = Propositions()
-        questionnaire.questionnaireID = questionnaireID
-        proposition.questionnaire = questionnaire
-        proposition.propositionTitle = propositionTitle
-        proposition.propositionSubTitle = propositionSubTitle
-        proposition.propositionDescription = propositionDescription
-        proposition.propositionAuthor = propositionAuthor
-        proposition.propositionLink = propositionLink
-
-    #Instancia Parlamentarians
-    def generateParlamentariansInstance(self, parlamentaryPhotoPath, parlamentaryUF, parlamentaryName, parlamentaryPoliticalParty):
-        parlamentary = Parlamentarians()
-        parlamentary.parlamentaryPhotoPath = parlamentaryPhotoPath
-        parlamentary.parlamentaryUF = parlamentaryUF
-        parlamentary.parlamentaryName = parlamentaryName
-        parlamentary.parlamentaryPoliticalParty = parlamentaryPoliticalParty

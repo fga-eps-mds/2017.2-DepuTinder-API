8bb803e8741f86c23220afb4cf6b3f82159a790d
vitorcx <vitorcx010@gmail.com>
2017-10-30 12:41:11 -0200

    Refactoring parserVotingData

--- votings/management/commands/parserVotingData.py
+++ votings/management/commands/parserVotingData.py
@@ -4,9 +4,7 @@ from questionnaire.models import Questionnaire
 from parlamentarians.models import Parlamentarians
 from django.core.management import BaseCommand
 from faker import Faker
-from strgen import StringGenerator
 import random
-
 
 fake = Faker()
 
@@ -15,14 +13,18 @@ class Command(BaseCommand):
         self.parseData()
 
     def parseData(self):
-        for i in range(10):
-            votings, created = Votings.objects.get_or_create(
-                #Gera inteiro para votação: -1 = NAO / 0 = ME ABSTENHO / 1 = SIM
-                candidateVote = random.randrange(-1,2),
-                candidateID = 
-                propositionID =
-            )
-            if (created):
-                self.stdout.write("Voting " + str(votings.candidateVote) + " salvo com sucesso!")
-            else:
-                self.stdout.write("Voting " + str(votings.candidateVote) + " erro ao salvar!")
+        #Para cada parlamentar são gerados 10 votos, cada voto em uma proposição diferente que tem seu id incrementado
+        #com a variavel j
+        for i in range(10):#trocar 10 pelo numero de parlamentares existentes
+            parlamentary = Parlamentarians.objects.get(id=i)
+            for j in range(10):
+                votings, created = Votings.objects.get_or_create(
+                    #Gera inteiro para votação: -1 = NAO / 0 = ME ABSTENHO / 1 = SIM
+                    candidateVote = random.randrange(-1,2),
+                    candidateID = parlamentary,
+                    propositionID = Propositions.objects.get(id=j)
+                )
+                if (created):
+                    self.stdout.write("Voting " + str(votings.candidateVote) + " salvo com sucesso!")
+                else:
+                    self.stdout.write("Voting " + str(votings.candidateVote) + " erro ao salvar!")

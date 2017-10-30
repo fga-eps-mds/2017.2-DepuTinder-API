811eac4f82308b19f1b7ba5e98e77931e43273f8
byronkamal <byronkamal@hotmail.com>
2017-10-30 20:50:49 -0200

    Changing parserQuestionnaireData

--- questionnaire/management/commands/parserQuestionnaireData.py
+++ questionnaire/management/commands/parserQuestionnaireData.py
@@ -1,24 +1,23 @@
 from questionnaire.models import Questionnaire
+from propositions.models import Propositions
 from django.core.management import BaseCommand
 from faker import Faker
 from random import randint
 
-fake = Faker()
-
-QUANTITY_OF_DATA = 10
-MIN_ID = 0
-MAX_ID = 100
-
+MIN_ID_QUESTIONNAIRE = 0
+MAX_ID_QUESTIONNAIRE = 1
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.parseData()
 
     def parseData(self):
+
+        proposition = (Propositions.objects.all()[:10].filter())
+
         # Data to be parsed
-        for i in range(QUANTITY_OF_DATA):
-            questionnaire, created = Questionnaire.objects.get_or_create(
-                questionnaireID = randint(MIN_ID, MAX_ID)
-            )
+        questionnaire, created = Questionnaire.objects.get_or_create(
+            questionnaireID = randint(MIN_ID_QUESTIONNAIRE, MAX_ID_QUESTIONNAIRE)
+        )
 
         if (created):
             self.stdout.write("Questionario " + str(questionnaire.questionnaireID) + " salvo com sucesso!")

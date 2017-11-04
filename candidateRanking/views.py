e4b3cfc65300a8c53b9219ac37bad6d16ccf4361
Calebe Rios <calebe_rios@hotmail.com>
2017-11-04 14:41:36 -0200

    Adding a function to transform a dict to json

--- candidateRanking/views.py
+++ candidateRanking/views.py
@@ -73,15 +73,15 @@ def generate_match(parlamentarians, answeredQuestions, super_match, high_match, 
         elif count >= 1 and count < 3:
             low_match.append(parlamentary)
 
-
-
+def toJson(parlamentary):
+    return json.dumps(parlamentary, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
 def generate_results(super_match, high_match, medium_match, low_match):
     results = {
-    "70% - 100%": super_match,
-    "50% - 70%": high_match,
-    "30% - 50%": medium_match,
-    "10% - 30%": low_match,
+    "70% - 100%": toJson(super_match),
+    "50% - 70%": toJson(high_match),
+    "30% - 50%": toJson(medium_match),
+    "10% - 30%": toJson(low_match),
     }
     print(results)
     return results

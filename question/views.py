3e28be18c0daea20075feebbca7fdc2bededd768
Amanda Muniz <amanda.muniiz@outlook.com>
2017-11-07 23:53:48 -0200

    Creating view for question

--- question/views.py
+++ question/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from .models import Question
+from .serializers import QuestionSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from django.core import serializers
+from rest_framework.decorators import api_view
+import requests, json
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def question(request):
+
+    if request.method == 'GET':
+        question = Question.objects.all()
+        if len(question) == 0:
+            return Response({}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            serializer = serializers.serialize('json', question)
+
+            return Response(serializer, status=status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        if request.data:
+            question, created = Question.objects.get_or_create(
+                questionTitle = request.data['questionTitle'],
+                questionSubtitle = request.data['questionSubtitle'],
+                questionDescription = request.data['questionDescription'],
+                questionAuthor = request.data['questionAuthor'],
+                questionLink = request.data['questionLink'],
+            )
+
+            if created:
+                return Response(status=status.HTTP_200_OK)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)

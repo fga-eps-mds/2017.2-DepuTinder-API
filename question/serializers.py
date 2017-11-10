2e581e6dfa76bba3fa78f54e9602c3a74c7c91ea
Amanda Muniz <amanda.muniiz@outlook.com>
2017-11-10 21:19:13 -0200

    Adding foreign keys in question serializer

--- question/serializers.py
+++ question/serializers.py
@@ -2,10 +2,15 @@ from rest_framework import serializers
 from .models import Question
 
 class QuestionSerializer(serializers.Serializer):
+    propositionFK = serializers.RelatedField(source='proposition', read_only=True)
+    questionnaireFK = serializers.RelatedField(source='questionnaire', read_only=True)
+
     class Meta:
         model = Question
         fields = (
             'questionTitle',
             'questionSubtitle',
             'questionDescription',
-            'questionAuthor')
+            'questionAuthor',
+            'propositionFK',
+            'questionnaireFK')

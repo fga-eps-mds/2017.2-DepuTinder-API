c51a8864de76fcdb397dc8b47f625f372f58566e
Calebe Rios <calebe_rios@hotmail.com>
2017-11-14 15:38:48 -0200

    Allowing user to be null

--- users/models.py
+++ users/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 IMAGE_LENGTH = 5000
 
 class Users(models.Model):
-    user = models.OneToOneField(User)
+    user = models.ForeignKey(User, related_name='User', null=True)
     userImage = models.TextField(max_length = IMAGE_LENGTH, blank = True)

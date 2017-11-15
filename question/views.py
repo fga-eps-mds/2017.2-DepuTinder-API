e676aad7011302de9b235265ec2b118e369b6b4a
Amanda Muniz <amanda.muniiz@outlook.com>
2017-11-15 18:09:32 -0200

    Fixing view and serializer of question

--- question/views.py
+++ question/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.core import serializers
 from rest_framework.decorators import api_view
+from django.http import JsonResponse
 import requests, json
 
 @api_view(['GET', 'POST'])
@@ -17,9 +18,8 @@ def question(request):
         if len(question) == 0:
             return Response({}, status=status.HTTP_404_NOT_FOUND)
         else:
-            serializer = serializers.serialize('json', question)
-
-            return Response(serializer, status=status.HTTP_200_OK)
+            serializer = QuestionSerializer(question, many=True)
+            return JsonResponse(serializer.data, safe=False)
 
     elif request.method == 'POST':
 
@@ -33,7 +33,7 @@ def question(request):
                 questionAuthor = request.data['questionAuthor'],
                 proposition = propo,
             )
-            
+
             if created:
                 return Response(status=status.HTTP_200_OK)
             else:

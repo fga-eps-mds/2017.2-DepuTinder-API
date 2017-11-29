8c0ecbe3447cb2917ec240304bd921226446a73f
CalebeRios <calebe_rios@hotmail.com>
2017-11-29 19:05:41 -0200

    Separating username in first_name and last_name

--- users/views.py
+++ users/views.py
@@ -34,7 +34,16 @@ def users(request):
             if len(user) > 0:
                 return Response(status=status.HTTP_400_BAD_REQUEST)
             else:
-                user = User.objects.create_user(request.data['userName'], request.data['userEmail'], request.data['userPassword'])
+                name = request.data['userName'].split(' ')
+                FLAG = True
+                lastName = ''
+
+                for i in name:
+                    if not FLAG:
+                        lastName += i + ' '
+                    FLAG = False
+
+                user = User.objects.create_user(first_name=name[0], email=request.data['userEmail'], password=request.data['userPassword'], last_name=lastName, username=request.data['userName'])
                 user.save()
 
                 users, created = Users.objects.get_or_create(
@@ -56,14 +65,17 @@ def users(request):
         if not u:
             return JsonResponse({"status": 500, "message": "Email não existe!"}, status=status.HTTP_400_BAD_REQUEST)
         else:
-            user_authenticate = authenticate(username=u[0].username, password=request.data['userPassword'])
+            user_authenticate = authenticate(first_name=u[0].first_name, password=request.data['userPassword'])
 
             if user_authenticate is not None:
                 user = Users.objects.get(user=user_authenticate.id)
+
                 seri = {"data": {
-                            "userName": user.user.username,
+                            "userName": user.user.first_name,
+                            "userLastName": user.user.last_name,
                             "userEmail": user.user.email,
                             "userImage": user.userImage,
+                            "admin": user.user.is_superuser,
                         },
                         "status": 200,
                         "message": "Entrou!"
@@ -86,16 +98,16 @@ def updateUser(request):
     if not u:
         return JsonResponse({"status":500, "message": "Email não existe!"}, status=status.HTTP_400_BAD_REQUEST)
     else:
-        user_authenticate = authenticate(username=u[0].username, password=request.data['oldUserPassword'])
+        user_authenticate = authenticate(first_name=u[0].first_name, password=request.data['userPassword'])
         if user_authenticate is not None:
             user = Users.objects.get(user=user_authenticate.id)
-            u[0].username = request.data['userName']
+            u[0].first_name = request.data['userName']
             u[0].email = request.data['userEmail']
             print(request.data['userPassword'])
             if(request.data['userPassword'] != ''):
                 u[0].set_password(request.data['userPassword'])
             u[0].save()
-            print(u[0].username)
+            print(u[0].first_name)
             print(u[0].email)
             print(u[0].password)
             return JsonResponse({"status":400, "message": "Email ou senha incorretos"}, status=status.HTTP_400_BAD_REQUEST)
